6e824799f5a3181ccfacb21c967041acc745c903
Subhajit Rajak <subhajitrajak2001@gmail.com>
2026-02-15 05:52:11 +0530

    Chore: Bump version code and name
    This commit updates the application's version information in `app/build.gradle.kts`.
    
    The `versionCode` has been incremented from 21 to 22, and the `versionName` has been updated from "1.1.0" to "1.2.0".

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "com.subhajitrajak.durare"
         minSdk = 26
         targetSdk = 36
-        versionCode = 21
-        versionName = "1.1.0"
+        versionCode = 22
+        versionName = "1.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
